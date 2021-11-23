5500531b2624086c9283791f4590f9e415cfc2ce
Cindy Zhang <58195295+cindyxinyuzhang@users.noreply.github.com>
2021-11-22 22:46:23 -0500

    add 0973

--- 0973 K Closest Points to Origin.py
+++ 0973 K Closest Points to Origin.py
@@ -2,3 +2,26 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         points.sort(key= lambda pair: sqrt(pair[0]**2+pair[1]**2))
         return points[:k]
+
+
+    
+    
+from heapq import heapify, heappush, heappop
+import math
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        def dist(x, y):
+            return math.sqrt(x**2+y**2)
+        
+        points = [[dist(x,y), [x,y]] for x, y in points]
+        heapify(points)
+        
+        ans = []
+        
+        for _ in range(k):
+            d, coords = heappop(points)
+            ans.append(coords)
+            
+        return ans    
